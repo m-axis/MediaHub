e1fdbfcc323a9e9d4461f8877230a4aa72f3c452
Makhsus Ali <makhsus.ali@sobeys.com>
2022-07-30 17:24:40 -0400

    PDF manager Logic implemented
    Implemented the main logic for PDF manager.

--- element_generator.py
+++ element_generator.py
@@ -4,15 +4,18 @@ file_browse_count = 0
 
 BLANK_PDF = b'iVBORw0KGgoAAAANSUhEUgAAASwAAADICAIAAADdvUsCAAAGr0lEQVR4nO3cQZLTSBAFUM/E3JIl+7kCd2DPknvOwgTjELYsS1WVmVXv7SG6CX7kV6atv759+3YD4vwd/QPA6oQQggkhBBNCCCaEEEwIIZgQQjAhhGBCCMGEEIIJIQQTQggmhBBMCCGYEEIwIYRgQgjBhBCCCSEE++fKH/73x89WPwdU8f3rl7Z/oUkIwYQQggkhBBNCCHZpMfNW80dY6Cpk19g3hPdfSRTJL3DVP6KOumSQ2b8/fu78Fx0wQlqGcOfH3f89Icp+/MaUuMaTcP/nlkPyCB+Av3V5Jvz+9curX89TIuH2h8H4/5y9FjP330QUySbJ9HvUdzGjnZJHnv650fdEcaedEitb/9wYEcKbdkqcnNPv0dCPrWmnjJS2f24EfHbUOZEBMhwAD4r5ALeRSD9VBuBvg54Jn9p5UPSUyAnJFzCvxH+VSTuliUL9cyM+hDftlGvK9c+NyDq64ZzIp4r2z41EIbw5J/KJ0tPvUYo6uqGdsq96/9zIGMI7CxueqruAeSVXHd3QTnk0xxPgn1KH8O5tFOv+63PQrPG7y1tHN7TTZc3XPzfKhPBmYbOeyRYwrxSooxvOiSuYu39u1AvhzcJmditMv0eV6uiGdjqfRfrnRslJ+Eg7ncNS/XOjfAhv2ml9C06/R4Xr6IZ2WtGa/XNjnhDeOScWMv0B8KDZQngzEiswAB/N8Ez4lHdn5LTyAuaVCSfhI+00Ff3zqclDeNNOc9A/d0xbRzecE6Pon2+tEsKbc2IE0++I+evohnY6hv553HIhvLOw6coC5iML1dEN7bQHT4AnrBvCO+/OaEX8Tlu0jm5opxfpn1cI4S8WNudYwFy3eh3dcE48Tv9sRQi3LGyOMP0aUkef005f0T+bE8I9FjYbFjA9qKNvaKd3pl8/JuEhK7dT/bM3IfzAgu1U/xxACD+zzkg0AIfxTHjG3OdEB8DBhPCkWRc2pt946uglM7VT/TOKSdhA9Xaqf8YSwjbqtlPTL5w62lKtdqp/JiGE7ZU4JzoA5iGEXWQeiQZgNp4JO8r2Kn4LmJxMwu6StFP9My0hHCG2neqfyamj44w/J+qfJQjhUCPPiaZfFepogN7tVP+sRQjDdFrYWMCUo45GattOTb+iTMJ419up/lmaEGZxup3qn9UJYSKfjkQDcA6eCdM5ck50AJyJEGZ0ZGGz8wepRR3N69MnOgksSgizOxItC5jShHAGSb4ozDlCmNrxj87IYV1CmNenucrz7gw+IoQZvT0Apn13Bic4UeRy/ACY7d0ZnGYSJnJi+iV5dwZXCGEKVz6App1Wp44Ga/UBtOqv4l+ZEEZq+wG0uq/iX5w6GqPfFyC003KEMMCAbwBa2BSijg418gsQ2mkVJuEgUV/A1U7zE8IRwt9AoZ1mJoR95XkDhZGYlmfCXnK+gcI5MSEh7CLJ9Nv5AUQxD3W0sTz9c592modJ2EzO/rlPO81ACNsoMf2e0k7DqaNXVemf+7TTQEJ4SfgBsC3nxBBCeNIcA/BPRuJ4ngk/VnEB8ynvzhjJJPzMZP1zn3Y6hhAeNWv/3KedDqCOvrdC/9znnNiVEL6x4PR7yjmxH3X0pTX75z7ttAchfG6pBcynLGzaUke3PAEeoZ02JIT/E79PvY2if7Qj1NFf9M/TtNOLhNACpgELmyuWrqP6Z1vOieesG0LTrwcLmxNWrKP6Z2/a6UfWmoT650ja6UELhdD0G087PWKJOqp/xtJO980fQgfAJJwTX5k5hAZgNkbiU3M+E1rAZGZhszFhCE2//CxsHk1VR/XPWrTTu0kmof5Zl3Y6QwhNv+oWb6e166j+OZNl22nhEDoATmnBc2LJOuoJcG6rtdNiIRS/dazz7oxKdVT/XNAK7bRGCC1gVjb9wiZ7HdU/uZv4nJg6hKYfj2Zd2CSto/onr8zXTjOG0AKGt2Za2OSqo6Yfx03TTrNMQv2TcyZopylCqH9yUel2GhxCA5BW6o7EsGdCB0B6qHhOjAmh6Uc/5RY2o+uo/skYhdrpuEmofzJeiXY6KISmH1Hyt9PudVT/JIPM7bRvCB0ASSXnObFXCA1Acko4Ets/E1rAkN/Og+L4p8TGk1D/pJAk7bRlCPVPysnQTrtvRw1A8ovNYcc7oexRyP45satek1ACqSikuLUPof5JdYP/A48+UQAbKb5ZDysTQggmhBBMCCHYpcWMLShcZxJCMCGEYEIIwYQQggkhBBNCCCaEEEwIIZgQQjAhhGBCCMGEEIIJIQQTQggmhBBMCCGYEEIwIYRgQgjB/gMN8hFwTj1WoQAAAABJRU5ErkJggg=='
 
+VIEW_COL_SIZE = (320, 600)
+BROWSE_BTN_SIZE = (38, 2)
+
 
 def file_browser(key, **kwargs):
     global file_browse_count
     file_browse_count += 1
     return [[sg.Input('', key=key, readonly=True, visible=False, **kwargs)],
-            [sg.FileBrowse(f'Open PDF {file_browse_count}', target=key, size=(40, 2))]]
+            [sg.FileBrowse(f'Open File {file_browse_count}', key=f"BROWSE-{key}", target=key, size=BROWSE_BTN_SIZE)]]
 
 
-def image(key, **kwargs):
+def add_image(key, **kwargs):
     return [[sg.Frame('Selected', [[sg.Checkbox('', default=True, key=f"CHECK-{key}", enable_events=True),
                                     sg.Button(" ⥣ ", key=f"UP-{key}"),
                                     sg.Button(" ⥥ ", key=f"DOWN-{key}"),
@@ -20,38 +23,39 @@ def image(key, **kwargs):
                       key=f"FRAME-{key}")]]
 
 
-browser_columns = [
-    [sg.Column([[sg.Input('', key="PF-FILENAME-0", readonly=True, visible=False, enable_events=True)],
-                [sg.FileBrowse(f'Open PDF 1', key="BROWSE-PF-FILENAME-0", target="PF-FILENAME-0", size=(40, 2))]]),
-     sg.Column([[sg.Input('', key="PF-FILENAME-1", readonly=True, visible=False, enable_events=True)],
-                [sg.FileBrowse(f'Open PDF 2', key="BROWSE-PF-FILENAME-1", target="PF-FILENAME-1", size=(40, 2),
-                               visible=False)]])
-     ]
+view_col_0 = [
+    sg.Column([
+        [sg.Image(source=BLANK_PDF, key="PF-IMAGE-PLACEHOLDER-0")]], key="PF-VIEW-COLUMN-0", size=VIEW_COL_SIZE,
+        scrollable=True, vertical_scroll_only=True),
 ]
 
-view_layout0 = [
-    [sg.Image(source="", key="PF-IMAGE-PLACEHOLDER0")],
-]
-view_layout1 = [
-    [sg.Image(source="", key="PF-IMAGE-PLACEHOLDER1")],
+browse_file_0 = [sg.Input('', key="PF-FILENAME-0", readonly=True, visible=False, enable_events=True),
+                 sg.FileBrowse(f'Load File 1', key="BROWSE-PF-FILENAME-0", target="PF-FILENAME-0",
+                               size=BROWSE_BTN_SIZE)]
+browse_file_1 = [sg.Input('', key="PF-FILENAME-1", readonly=True, visible=False, enable_events=True),
+                 sg.FileBrowse(f'Load File 2', key="BROWSE-PF-FILENAME-1", target="PF-FILENAME-1",
+                               size=BROWSE_BTN_SIZE, disabled=True)]
+
+view_col_1 = [
+    sg.Column([
+        [sg.Image(source=BLANK_PDF, key="PF-IMAGE-PLACEHOLDER-1")]], key="PF-VIEW-COLUMN-1", size=VIEW_COL_SIZE,
+        scrollable=True, vertical_scroll_only=True),
 ]
 
 action_layout = [
     [sg.Frame('',
-              [[sg.Button("Save PDF", key="PF-SAVE"),
-                sg.Button("Preview", key="PF-PREVIEW")]
+              [[sg.Button("Save PDF", key="PF-SAVE", disabled=True),
+                sg.Button("Preview", key="PF-PREVIEW", disabled=True),
+                ],
                ]
-              , key="FRAME-PF-ACTION", vertical_alignment='c', visible=False)]
+              , key="FRAME-PF-ACTION", vertical_alignment='c', size=(VIEW_COL_SIZE[0] * 2 + 66, 40))]
 ]
 
 layout = [
-    [sg.Frame('', browser_columns, vertical_alignment='t')],
     action_layout,
     [
-        sg.Column(view_layout0, key="PF-VIEW-COLUMN-0", size=(320, 600), scrollable=True, vertical_scroll_only=True,
-                  visible=False),
-        sg.Column(view_layout1, key="PF-VIEW-COLUMN-1", size=(320, 600), scrollable=True, vertical_scroll_only=True,
-                  visible=False)
+        [sg.Frame('File 1', [browse_file_0, view_col_0]),
+         sg.Frame('File 2', [browse_file_1, view_col_1])],
 
     ],
 
--- pdf_manager.py
+++ pdf_manager.py
@@ -1,79 +1,117 @@
 from element_generator import *
+from collections import OrderedDict
 from common import *
-import re
 
 window = sg.Window("PDF", layout)
-page_counter0 = 0
-page_counter1 = 0
-selected_pages = [[],[]]
-all_images = [[], []]
+col_img = [OrderedDict(), OrderedDict()]
+IMG_SRC = 0
+IMG_CHK = 1
+IMG_POS = 2
+
+
+def update(key, **kwargs):
+    window[key].update(**kwargs)
+
+
+def refresh(key):
+    window.refresh()  # refresh required here
+    window[key].contents_changed()
+
+
+def all_keys():
+    return window.key_dict.keys()
+
+
+def clear_col_img(col):
+    a_ks = all_keys()
+    for a_k in a_ks:
+        if f"IMAGE-PF-PAGE-{col}-" in a_k and not "-IMAGE" in a_k:
+            act_img(a_k, dormant=True)
+    if col_img[col]:
+        col_img[col] = OrderedDict()
+
+
+def act_img(i_key, visible=True, dormant=False):
+    global col_img
+    col = int(i_key.split("-")[-2])
+    frame_key = f'FRAME-{i_key}'
+    up_key = f"UP-{i_key}"
+    down_key = f"DOWN-{i_key}"
+    check_key = f"CHECK-{i_key}"
+    if not dormant:
+        col_img[col][i_key][IMG_CHK] = visible
+        update(i_key, source=col_img[col][i_key][IMG_SRC] if visible else BLANK_PDF)
+        update(frame_key, value="Selected" if visible else "")
+        update(up_key, visible=visible)
+        update(down_key, visible=visible)
+        update(check_key, value=visible)
+    else:
+        if i_key in col_img[col].keys():
+            col_img[col][i_key][IMG_CHK] = False
+            col_img[col][i_key][IMG_SRC] = ""
+        update(check_key, value=False)
+    update(frame_key, visible=not dormant)
+
+
+def on_file_load(file_path, col):
+    global col_img
+    images = pdf_to_image(file_path)
+    a_ks = all_keys()
+    for a_k in a_ks:
+        if f"IMAGE-PF-PAGE-{col}-" in a_k and not "-IMAGE" in a_k:
+            act_img(a_k, dormant=True)
+    if col_img[col]:
+        col_img[col] = OrderedDict()
+    # GUI Updates
+    update(f'PF-IMAGE-PLACEHOLDER-{col}', source='')
+    update('BROWSE-PF-FILENAME-1', disabled=False)
+    update('PF-SAVE', disabled=False)
+    update('PF-PREVIEW', disabled=False)
+    i = 0
+    for img_b in images:
+        img_k = f"IMAGE-PF-PAGE-{col}-{str(i)}"
+        col_img[col][img_k] = []
+        col_img[col][img_k].append(img_b)
+        col_img[col][img_k].append(True)
+        col_img[col][img_k].append(i)
+        if img_k in a_ks:
+            act_img(img_k)
+            print(f"Found image key: {img_k}")
+        else:
+            i_key = f"IMAGE-PF-PAGE-{col}-{str(i)}"
+            window.extend_layout(window[f'PF-VIEW-COLUMN-{col}'],
+                                 add_image(i_key,
+                                           source=img_b,
+                                           enable_events=True))
+            act_img(i_key)
+            print(f"New image key: {i_key}")
+        i += 1  # Increase counter for current file image key
+    refresh(f'PF-VIEW-COLUMN-{col}')
+
+
+def on_preview():
+    global col_img
+
+
 while True:
     event, values = window.read()
     if event in ('-exit-', None):
         break
     elif 'PF-FILENAME-0' in event:
-        images = pdf_to_image(values[event])
-        all_images[0] = images
-        window['PF-VIEW-COLUMN-0'].update(visible=True)
-        window['FRAME-PF-ACTION'].update(visible=True)
-        window['BROWSE-PF-FILENAME-1'].update(visible=True)
-        for image_b in images:
-            window.extend_layout(window['PF-VIEW-COLUMN-0'],
-                                 image(f"IMAGE-PF-PAGE-0-{str(page_counter0)}", source=image_b, enable_events=True))
-            page_counter0 += 1
-        window.refresh()  # refresh required here
-        window['PF-VIEW-COLUMN-0'].contents_changed()
+        on_file_load(values[event], 0)
     elif 'PF-FILENAME-1' in event:
-        images = pdf_to_image(values[event])
-        all_images[1] = images
-        window['PF-VIEW-COLUMN-1'].update(visible=True)
-        window['FRAME-PF-ACTION'].update(visible=True)
-        for image_b in images:
-            window.extend_layout(window['PF-VIEW-COLUMN-1'],
-                                 image(f"IMAGE-PF-PAGE-1-{str(page_counter1)}", source=image_b, enable_events=True))
-            print(f"IMAGE-PF-PAGE-{str(page_counter1)}")
-            page_counter1 += 1
-        window.refresh()  # refresh required here
-        window['PF-VIEW-COLUMN-1'].contents_changed()
+        on_file_load(values[event], 1)
     elif "CHECK-IMAGE-PF-PAGE-" in event:
-        image_index = int(event.split("-")[-1])
-        loc_index = int(event.split("-")[-2])
-        image_key = event.replace("CHECK-", "")
-        frame_key = f'FRAME-{image_key}'
-        up_key = f"UP-{image_key}"
-        down_key = f"DOWN-{image_key}"
-        if values[event]:
-            image_source = all_images[loc_index][image_index]
-            frame_title = "Selected"
-        else:
-            image_source = BLANK_PDF
-            frame_title = ""
-        window[image_key].update(source=image_source)
-        window[frame_key].update(value=frame_title)
-        window[up_key].update(visible=values[event])
-        window[down_key].update(visible=values[event])
+        img_key = event.replace("CHECK-", "")
+        act_img(img_key, values[event])
+    elif "IMAGE-PF-PAGE-" in event and "-IMAGE-PF-PAGE" not in event:
+        img_key = event
+        chk_key = f"CHECK-{event}"
+        act_img(img_key, not values[chk_key])
+        # col = int(event.split("-")[-2])
+        # print(col_img[col][event])
     elif "UP-IMAGE-PF-PAGE-" in event or "DOWN-IMAGE-PF-PAGE-" in event:
         print("Up or Down")
-    elif "IMAGE-PF-PAGE-" in event:
-        image_index = int(event.split("-")[-1])
-        loc_index = int(event.split("-")[-2])
-        check_key = f'CHECK-{event}'
-        frame_key = f'FRAME-{event}'
-        up_key = f"UP-{event}"
-        down_key = f"DOWN-{event}"
-        if values[check_key]:
-            image_source = BLANK_PDF
-            frame_title = ""
-            checked = False
-        else:
-            image_source = all_images[loc_index][image_index]
-            frame_title = "Selected"
-            checked = True
-        window[frame_key].update(value=frame_title)
-        window[event].update(source=image_source)
-        window[check_key].update(value=checked)
-        window[up_key].update(visible=checked)
-        window[down_key].update(visible=checked)
     else:
         print(event)
 
